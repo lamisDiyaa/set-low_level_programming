82a9f95f94f93d2b8333cf4715af7116aebd9aff
lamisDiyaa <lamiiis.diyaa@gmail.com>
2026-04-18 02:26:11 -0700

    Add function-like macro ABS

---
+++ 0x0B-preprocessor/3-function_like_macro.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+/**
+ * File: 3-function_like_macro.h
+ * @x: The value to compute the absolute value of.
+ *
+ * Desc: Function-like macro ABS(x) that computes
+ * the absolute value of a number x.
+ */
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif
