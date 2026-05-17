4907c92e91a33111b95baa125cab9bce34296d42
lamisDiyaa <lamiiis.diyaa@gmail.com>
2026-05-17 12:52:31 -0700

    feat: implement 1-strdup function successfully

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -2,30 +2,41 @@
 #include <stdlib.h>
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory
- * @str: string to duplicate
- * Return: pointer to the duplicated string, or NULL if insufficient memory
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string, or NULL if str is NULL
+ * or if insufficient memory was available.
  */
 char *_strdup(char *str)
 {
-	char *duplicate;
-	unsigned int i, len = 0;
+char *dup;
+unsigned int i, len;
 
-	if (str == NULL)
-		return (NULL);
+/* الخطوة 1: التحقق إذا كان النص الممرر فارغاً */
+if (str == NULL)
+return (NULL);
 
-	while (str[len] != '\0')
-		len++;
+/* الخطوة 2: حساب طول النص */
+len = 0;
+while (str[len] != '\0')
+{
+len++;
+}
 
-	duplicate = malloc(sizeof(char) * (len + 1));
+/* الخطوة 3: حجز الذاكرة للنص الجديد (+1 للـ Null terminator) */
+dup = malloc(sizeof(char) * (len + 1));
 
-	if (duplicate == NULL)
-		return (NULL);
+/* الخطوة 4: التحقق من نجاح عملية حجز الذاكرة */
+if (dup == NULL)
+return (NULL);
 
-	for (i = 0; i < len; i++)
-		duplicate[i] = str[i];
+/* الخطوة 5: نسخ محتوى النص الأصلي إلى النص الجديد */
+for (i = 0; i <= len; i++)
+{
+dup[i] = str[i];
+}
 
-	duplicate[len] = '\0';
-
-	return (duplicate);
+return (dup);
 }
