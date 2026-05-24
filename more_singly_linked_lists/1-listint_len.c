aad507dd6cd665fd1f700c1ad258a66d42401e7f
lamisDiyaa <lamiiis.diyaa@gmail.com>
2026-05-24 16:04:47 -0700

    Fix coding style to match Kernel standards

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked listint_t list.
+ * listint_len - Returns the number of elements in a linked listint_t list.
  * @h: A pointer to the head of the listint_t list.
  *
  * Return: The number of elements in the list.
@@ -10,8 +10,7 @@ size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
-	{
+	while (h) {
 		count++;
 		h = h->next;
 	}
