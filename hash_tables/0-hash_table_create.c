79ea51f83866e38ce7030bb6f02e90bb5936dff9
lamisDiyaa <lamiiis.diyaa@yahoo.com>
2026-06-22 11:06:08 -0700

    Fix all files and betty style issues

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,12 +15,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size == 0)
 		return (NULL);
 
-	/* 1. حجز مكان لهيكل الجدول في الذاكرة */
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
-	/* 2. حجز مكان للمصفوفة التي ستحتوي على العناصر (المؤشرات) */
 	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 	{
@@ -30,7 +28,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ht->size = size;
 
-	/* 3. تهيئة كل خانة في المصفوفة لتشير إلى NULL */
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
 
