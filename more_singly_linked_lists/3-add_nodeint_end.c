688fa8d5acc2735910b242a19c615312875589b9
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-07-12 10:25:58 -0700

    Add 3-add_nodeint_end.c function

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,39 +3,38 @@
 
 /**
  * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: double pointer to the head of the list
- * @n: integer to be added in the new node
- *
- * Return: the address of the new element, or NULL if it failed
+ * @head: double pointer to the head of the listint_t list
+ * @n: integer value to be stored in the new node
+ * Return: address of the new element, or NULL if it failed
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node;
-	listint_t *temp;
+listint_t *new_node;
+listint_t *temp;
 
-	if (head == NULL)
-		return (NULL);
+if (head == NULL)
+return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+return (NULL);
 
-	new_node->n = n;
-	new_node->next = NULL;
+new_node->n = n;
+new_node->next = NULL;
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
 
-	temp = *head;
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
+temp = *head;
+while (temp->next != NULL)
+{
+temp = temp->next;
+}
 
-	temp->next = new_node;
+temp->next = new_node;
 
-	return (new_node);
+return (new_node);
 }
