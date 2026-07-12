82d86476c456ed83a98246e6c9b2c9f4b95896d8
lamisDiyaa <lamiiis.diyaa@gmail.com.com>
2026-07-12 10:48:28 -0700

    Force overwrite and fix compilation error for 7-get_nodeint.c

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -1,22 +1,13 @@
-#include <stdlib.h>
 #include "lists.h"
-
-/**
- * get_nodeint_at_index - returns the nth node of a listint_t linked list
- * @head: pointer to the head of the list
- * @index: index of the node
- * Return: pointer to the node, or NULL
- */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i = 0;
-
-	while (head != NULL)
-	{
-		if (i == index)
-			return (head);
-		head = head->next;
-		i++;
-	}
-	return (NULL);
+unsigned int i = 0;
+while (head != NULL)
+{
+if (i == index)
+return (head);
+head = head->next;
+i++;
+}
+return (NULL);
 }
